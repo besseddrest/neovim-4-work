62bb782b6cda22484b94e9e0370057dbe992395f
besseddrest <hcabalic@gmail.com>
2024-12-06 23:58:25 -0800

    revert noice dep

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -40,6 +40,6 @@ return {
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    "folke/snacks.nvim",
+    -- "folke/snacks.nvim",
   },
 }
